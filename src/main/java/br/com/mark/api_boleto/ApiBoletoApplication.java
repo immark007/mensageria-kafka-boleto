1ebf7e182c255f97013cc8841fec71f3c651d593
immark007 <immark5507@gmail.com>
2025-05-02 08:56:17 -0300

    feat: habilitando o kafka na minha aplicação

--- src/main/java/br/com/mark/api_boleto/ApiBoletoApplication.java
+++ src/main/java/br/com/mark/api_boleto/ApiBoletoApplication.java
@@ -2,8 +2,10 @@ package br.com.mark.api_boleto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
+@EnableKafka
 public class ApiBoletoApplication {
 
 	public static void main(String[] args) {
